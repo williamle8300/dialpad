d5551485b07a4bfbd57666f7f932c7f2b4045f7e
William Le <wle8300@gmail.com>
2017-05-20 13:21:35 -0700

    asdf

--- src/Key.js
+++ src/Key.js
@@ -11,25 +11,25 @@ module.exports = createReactClass({
     //Hack for non-standard HTML attributes (e.g., onTouchTap)
     //Reference: https://github.com/styled-components/styled-components/issues/527#issuecomment-281931998
     const Key = styled((props) => <button {...props}/>)`
-    margin: 0.8%;
-    width: ${100/3 - 2}%;
-    font-size: 1.25em;
-    font-family: "Open Sans", sans-serif;
-    color: ${!this.props.isDisabled ? '#76a28a' : '#2c8454'};
-    border: ${!this.props.isDisabled ? '1px solid #203c2c' : '1px solid #0d3c21'};
-    border-radius: 2px;
-    cursor: url('http://www.clker.com/cliparts/I/V/F/G/y/K/mouse-pointer-hi.png'), auto;
-    background-color: ${!this.props.isDisabled ? '#28332e' : '#1c211f'};
-    outline-style: none;
-    transition: all 100ms ease-out;
-    text-shadow: 0px 1px 6px rgba(0, 253, 114, 0.25);
-    &:active {
-      color: #555;
-      background-color: #24ce6e;
-    }
+      margin: 0.8%;
+      width: ${100/3 - 2}%;
+      font-size: 1.25em;
+      font-family: "Open Sans", sans-serif;
+      color: ${!this.props.isDisabled ? '#76a28a' : '#2c8454'};
+      border: ${!this.props.isDisabled ? '1px solid #203c2c' : '1px solid #0d3c21'};
+      border-radius: 2px;
+      cursor: url('http://www.clker.com/cliparts/I/V/F/G/y/K/mouse-pointer-hi.png'), auto;
+      background-color: ${!this.props.isDisabled ? '#28332e' : '#1c211f'};
+      outline-style: none;
+      transition: all 100ms ease-out;
+      text-shadow: 0px 1px 6px rgba(0, 253, 114, 0.25);
+      &:active {
+        color: #555;
+        background-color: #24ce6e;
+      }
     `
 
-    
+
     return <Key disabled={this.props.isDisabled} onTouchTap={this.props.handleTap}>{this.props.children}</Key>
   },
 })
